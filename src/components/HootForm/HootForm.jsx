f9ad079d9a4694d08e279daa971fd98a723882f8
erlopez11 <lopezelainy@gmail.com>
2025-04-01 15:40:04 -0500

    wip: complete boilerplate of hoot form

---
+++ src/components/HootForm/HootForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+const HootForm = (props) => {
+    const [formData, setFormData] = useState({
+        title: '',
+        text: '',
+        category: 'News',
+    });
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('formData', formData);
+    };
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='title-input'>Title</label>
+                <input
+                    required
+                    type='text'
+                    name='title'
+                    id='title-input'
+                    value={formData.title}
+                    onChange={handleChange}
+                />
+                <label htmlFor='text-input'>Text</label>
+                <textarea
+                    required
+                    type='text'
+                    name='text'
+                    id='text-input'
+                    value={formData.text}
+                    onChange={handleChange}
+                 />
+                 <label htmlFor='category-input'>Category</label>
+                 <select
+                    required
+                    name='category'
+                    id='category-input'
+                    value={formData.category}
+                    onChange={handleChange}
+                 >
+                    <option value="News">News</option>
+                    <option value="Games">Games</option>
+                    <option value="Music">Music</option>
+                    <option value="Movies">Movies</option>
+                    <option value="Sport">Sports</option>
+                    <option value="Television">Television</option>
+                 </select>
+                 <button type='submit'>Submit</button>
+            </form>
+        </main>
+    );
+
+};
+
+export default HootForm;
