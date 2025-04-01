e15e43fe499265c59ce887910239b0315e86ecd0
erlopez11 <lopezelainy@gmail.com>
2025-04-01 16:04:53 -0500

    wip:add navbar links

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router";
 
 const HootList = (props) => {
     return (
         <main>
-            Hoot List
+            {props.hoots.map((hoot) => (
+                <Link key={hoot._id} to= {`/hoots/${hoot._id}`}>
+                    <article>
+                        <header>
+                            <h2>{hoot.title}</h2>
+                            <p>{`${hoot.author.username} posted on ${new Date(hoot.createdAt).toLocaleDateString()}`}</p>
+                        </header>
+                        <p>{hoot.text}</p>
+                    </article>
+                </Link>
+            ))}
         </main>
     );
 };
