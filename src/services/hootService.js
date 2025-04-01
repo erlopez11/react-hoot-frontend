1f1afe1cff9a913d13f3aad5483e093e051502c7
erlopez11 <lopezelainy@gmail.com>
2025-04-01 16:05:54 -0500

    wip: add index, show, create for hootservice

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -14,4 +14,34 @@ const index = async () => {
     }
 };
 
-export{ index }; //named export syntax used to export multiple fns from a module
+const show = async (hootId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${hootId}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        });
+        return res.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const create = async (hootFormData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(hootFormData),
+        });
+        return res.json();
+    } catch (error) {
+        console.error(error);
+        
+    }
+};
+
+export{ index, show, create }; //named export syntax used to export multiple fns from a module
