cea1a4efa93fc7d70cceca205cc5d442f72d31e8
erlopez11 <lopezelainy@gmail.com>
2025-04-01 16:04:22 -0500

    wip: import hooks & add componets & add handleAddHoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext, useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 
 import { UserContext } from './contexts/UserContext';
 
+import * as hootService from './services/hootService';
+
 const App = () => {
   const { user } = useContext(UserContext);
-  
+  const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]); //updating state in ascending order
+    navigate('/hoots');
+  }
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots(); //Only fetch hoots when user is logged in
+  }, [user]); //effect fn depends on the user adding user dependency
+  //because effect depends on the user to run (if not included runs risk of infinite loop)
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        {user ? (
+          <>
+            <Route path="/hoots" element={<HootList hoots={hoots} />} />
+            <Route path="/hoots/:hootId" element={<HootDetails />}/>
+            <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+          </>
+        ) : (
+          <>
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
