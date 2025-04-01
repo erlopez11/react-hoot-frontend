aeea542f0d232abb81abd2b784a66de2d250bc14
erlopez11 <lopezelainy@gmail.com>
2025-04-01 15:18:33 -0500

    wip: finish hootDetails Componnet

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,9 +1,52 @@
+import { useState, useEffect } from 'react';
+//import * as hootService from '../../services/hootService';
+import { show } from '../../services/hootService'; //best way to import 
+import { useParams } from 'react-router';
+
 
 const HootDetails = (props) => {
+    const [hoot, setHoot] = useState(null);//null so can do conditional render if no hootdetails 
+    const { hootId } = useParams();
+
+    useEffect(() => {
+        const fetchHoot = async () => {
+            const hootData = await show(hootId);
+            setHoot(hootData)
+        };
+        fetchHoot(); //this will run when effect function runs
+        //the effect function runs when we have a hootId
+    }, [hootId]);
+
+    if (!hoot) return <main>Loading...</main>//show loading while hoot gets hootData
 
     return (
         <main>
-            Hoot details
+            <section>
+                <header>
+                    <p>{hoot.category.toUpperCase()}</p>
+                    <h1>{hoot.title}</h1>
+                    <p>
+                        {`${hoot.author.username} posted on ${new Date(hoot.createdAt).toLocaleDateString()}`}
+                    </p>
+                </header>
+                <p>{hoot.text}</p>
+            </section>
+            <section>
+                <h2>Comments</h2>
+
+                {!hoot.comments.length && <p>There are no comments.</p>}
+
+                {hoot.comments.map((comment) => (
+                    <article key={comment._id}>
+                        <header>
+                            <p>
+                                {`${comment.author.username} posted on ${new Date(comment.createdAt).toLocaleDateString()}`}
+                            </p>
+                        </header>
+                        <p>{comment.text}</p>
+                    </article>
+                ))}
+            </section>
         </main>
     );
 };
